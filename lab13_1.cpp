2d5e40dcbb869873b8c9c6c1dff9092f3ea0a2ca
Llmetine <ntjeddelog@gmail.com>
2024-12-31 16:34:20 +0700

    finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,26 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]) {
+    double sum=0,pro=1,hSum=0,max=A[0],min=A[0];
+    for (int i=0;i<N;i++) {
+        sum+=A[i];
+        pro*=A[i];
+        hSum+=1.0/A[i];
+        if (A[i]>max)max=A[i];
+        if (A[i]<min)min=A[i];
+    }
+
+    B[0]=sum/N;
+
+    double var=0;
+    for (int i=0;i<N;i++) {
+        var+=pow(A[i]-B[0],2);
+    }
+    B[1]=sqrt(var/N);
+    B[2]=pow(pro,1.0/N);
+    B[3]=N/hSum;
+    B[4]=max;
+    B[5]=min;
+}
